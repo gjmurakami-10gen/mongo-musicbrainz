b8d81c31f363c270aaf7893f4d2b088f3d487fb7
Gary Murakami <gary.murakami@10gen.com>
2014-02-21 18:16:28 -0500

    bson_metrics.rb - bson-ruby example/sample

---
+++ script/bson_metrics.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+# Copyright (C) 2009-2014 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+gem "bson", "~> 2.2.1"
+require 'bson'
+require 'json'
+require 'benchmark'
+
+module BSON
+  module Registry
+    @@hist = ::Array.new(256, 0)
+    def get(byte)
+      @@hist[byte.ord] += 1
+      MAPPINGS.fetch(byte)
+    end
+  end
+end
+
+ARGV.take(1).each do |file_name|
+  doc_count = 0
+  file = File.open(file_name)
+  bm = Benchmark.measure do
+    while !file.eof
+      Hash.from_bson(file)
+      doc_count += 1
+    end
+  end
+  hist = BSON::Registry.class_variable_get(:@@hist)
+  element_count = hist.inject{|sum,x| sum + x }
+  hash_count = hist[BSON::Hash::BSON_TYPE.ord]
+  array_count = hist[BSON::Array::BSON_TYPE.ord]
+  embed_count = hash_count + array_count
+  puts <<-EOT.gsub(/^    /, '')
+    seconds: #{'%.1f' % bm.real}
+    docs/sec: #{(doc_count.to_f/bm.real).round}
+    docs: #{doc_count}
+    elements: #{element_count}
+    embeds: #{embed_count}
+    denorm: #{'%.1f' % (embed_count.to_f / doc_count.to_f)}
+    degree: #{'%.1f' % (element_count.to_f / (doc_count + embed_count).to_f)}
+  EOT
+  counts = BSON::Registry::MAPPINGS.collect{|byte, klass| [klass, hist[byte.ord]]}
+  counts = counts.select{|elem| elem[1] > 0}
+  counts = counts.sort{|a,b| b[1] <=> a[1]}
+  puts counts.collect{|klass, count| "#{klass}: #{(100.0*count.to_f/element_count.to_f).round}%"}.join("\n")
+end
